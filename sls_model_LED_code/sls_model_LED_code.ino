c3edb9449d53b900b060389c9ab1c5d3e203564d
Austin Gerber <austinb.gerber@gmail.com>
2024-12-22 12:14:53 -0700

    Changed state machine. LEDs stay on indefinitely. Liftoff indef.

--- sls_model_LED_code/sls_model_LED_code.ino
+++ sls_model_LED_code/sls_model_LED_code.ino
@@ -6,6 +6,7 @@
 // Define button debouncer values
 #define NUM_CHECKS 10
 #define CHECK_DELAY 2
+#define DEBOUNCE_DELAY 500
 
 // Define the pin connected to the NeoPixel strip and the number of LEDs
 #define NEO_PIN 5
@@ -34,8 +35,6 @@ typedef enum {
 
 // Define liftoff routine values
 #define LIFTOFF_BUTTON_WAIT 500   //milliseconds
-#define LIFTOFF_ITR_MIN 25
-#define LIFTOFF_ITR_MAX 50
 #define LIFTOFF_OFF_MIN 10
 #define LIFTOFF_OFF_MAX 50
 #define LIFTOFF_ON_MIN 50
@@ -56,33 +55,23 @@ void setup() {
 }
 
 void loop() {
-  static bool button_press = 0;
   static state_t state = STATE_OFF;
 
-  if (digitalRead(BUTTON_PIN) == 0) {     // Active low button
-    button_press = debounce(BUTTON_PIN);
-    LED_set(FULL);    // DEBUG
+  // State machine
+  switch (state) {
+    case STATE_OFF:
+      state = state_off();
+      break;
+    case STATE_ON:
+      state = state_on();
+      break;
+    case STATE_LIFTOFF:
+      state = state_liftoff();
+      break;
+    default:
+      state = state_on();         // If broken assume was on, change state to off
+      break;
   }
-
-  if (button_press == 1) {          // Change state if the button is pressed
-    switch (state) {                // State machine
-      case STATE_OFF:
-        state = state_off();
-        break;
-      case STATE_ON:
-        state = state_on();
-        break;
-      case STATE_LIFTOFF:
-        state = state_liftoff();
-        break;
-      default:
-        state = state_on();         // If broken assume was on, change state to off
-        break;
-    }
-  }
-
-  while(debounce(BUTTON_PIN));      // Wait until button is unpressed
-  button_press = 0;
 }
 
 
@@ -93,9 +82,11 @@ bool debounce(int button) {
   bool debounce = 0;
 
   for (int i = 0; i < NUM_CHECKS; i++) {
-    reading += !digitalRead(button);      // Active low button so not it to add up
+    reading += !digitalRead(button);      // Active low button so NOT it to add up
     delay(CHECK_DELAY);
   }
+
+  delay(DEBOUNCE_DELAY);
 
   return debounce = (reading == NUM_CHECKS) ? 1 : 0;
 }
@@ -139,9 +130,7 @@ void LED_set(intensity_t intensity) {
       break;
   }
 
-  for (int i = 0; i < NUMPIXELS; i++) {       // Set LEDs
-    pixels.setPixelColor(i, pixels.Color(red, green, blue));
-  }
+  pixels.fill(pixels.Color(red, green, blue));
   pixels.show();
 }
 
@@ -152,19 +141,25 @@ state_t state_off(void) {
   state_t next_state = STATE_OFF;
   bool button_press = 0;
 
-  // Change state to either STATE_ON or STATE_LIFTOFF
-  LED_set(FULL);                                 // Turn on LEDs
-  
-  // Check if button is long-pressed
-  delay(LIFTOFF_BUTTON_WAIT);
+  LED_set(OFF);
+
   button_press = debounce(BUTTON_PIN);
 
-  if (button_press == 0) {                    // If button was regular-pressed
-    next_state = STATE_ON;
-  }
-  else {                                      // If button was long-pressed
-    liftoff_routine();
-    next_state = STATE_LIFTOFF;
+  // Change state to either STATE_ON or STATE_LIFTOFF
+  if (button_press) {
+    LED_set(FULL);                                 // Turn on LEDs
+  
+    // Check if button is long-pressed
+    // delay(LIFTOFF_BUTTON_WAIT);      // TEST
+    button_press = debounce(BUTTON_PIN);
+
+    if (button_press == 0) {                    // If button was regular-pressed
+      next_state = STATE_ON;
+    }
+    else {                                      // If button was long-pressed
+      next_state = STATE_LIFTOFF;
+      while (digitalRead(BUTTON_PIN) == 0);    // TEST // Wait until button is unpressed (active low button)
+    }
   }
 
   return next_state;
@@ -172,20 +167,35 @@ state_t state_off(void) {
 
 state_t state_on(void) {
   state_t next_state = STATE_ON;
+  bool button_press = 0;
+
+  LED_set(FULL);
+
+  button_press = debounce(BUTTON_PIN);
 
   // Change state to STATE_OFF
-  LED_set(OFF);
-  next_state = STATE_OFF;
+  if (button_press) {
+    LED_set(OFF);
+    next_state = STATE_OFF;
+  }
+
   return next_state;
 }
 
 state_t state_liftoff(void) {
   state_t next_state = STATE_LIFTOFF;
+  bool button_press = 0;
+
+  liftoff_routine();
+
+  button_press = debounce(BUTTON_PIN);
 
   // Change state to STATE_OFF
-  liftoff_routine();
-  LED_set(OFF);
-  next_state = STATE_OFF;
+  if (button_press) {
+    LED_set(OFF);
+    next_state = STATE_OFF;
+  }
+
   return next_state;
 }
 
@@ -193,12 +203,10 @@ state_t state_liftoff(void) {
 
 // Liftoff routine
 void liftoff_routine(void) {
-  for (int j = 0; j < random(LIFTOFF_ITR_MIN, LIFTOFF_ITR_MAX); j++) {
-    // LED_set(HALF);
-    LED_set(THREE_QUARTER);
-    delay(random(LIFTOFF_OFF_MIN, LIFTOFF_OFF_MAX));
+  // LED_set(HALF);
+  LED_set(THREE_QUARTER);
+  delay(random(LIFTOFF_OFF_MIN, LIFTOFF_OFF_MAX));
 
-    LED_set(FULL);
-    delay(random(LIFTOFF_ON_MIN, LIFTOFF_ON_MAX));
-  }
+  LED_set(FULL);
+  delay(random(LIFTOFF_ON_MIN, LIFTOFF_ON_MAX));
 }
